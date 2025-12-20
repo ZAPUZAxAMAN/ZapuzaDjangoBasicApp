b949b8833fac2e4fada3d67f94716cd5d63178fe
TSSLXAMAN <amancse@student.iul.ac.in>
2025-12-20 19:47:29 +0530

    Deployment settings complete

--- ZapuzaDjangoBasicApp/settings.py
+++ ZapuzaDjangoBasicApp/settings.py
@@ -103,6 +103,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# `STATICFILES_DIRS` contains locations for your source static files (already set above).
+# `STATIC_ROOT` must be a separate directory where `collectstatic` will place files.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
